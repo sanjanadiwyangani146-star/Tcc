c9468421cd3387a8d769da4a1f763fc9254a5e87
sanjanadiwyangani146-star <sanjanadiwyangani146@gmail.com>
2026-03-18 19:47:42 +0530

    Add files via upload

---
+++ index.js
@@ -0,0 +1,33 @@
+const express = require('express');
+const app = express();
+__path = process.cwd()
+const bodyParser = require("body-parser");
+const PORT = process.env.PORT || 8000;
+let code = require('./pair'); 
+
+require('events').EventEmitter.defaultMaxListeners = 500;
+
+/* ---------- ROUTES ---------- */
+app.use('/code', code);
+app.use('/pair', async (req, res, next) => {
+    res.sendFile(__path + '/pair.html')
+});
+app.use('/', async (req, res, next) => {
+    res.sendFile(__path + '/main.html')
+});
+
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
+
+/* ---------- SERVER ---------- */
+app.listen(PORT, () => {
+    console.log(`
+╔─  Tʜᴇ Cᴏᴅᴇꜱ Cʀᴇᴡ Mɪɴɪ  ─╗
+║ Dᴏɴ'ᴛ Fᴏʀɢᴇᴛ Gɪᴠᴇ A Sᴛᴀʀ⭐
+║
+║ Sᴇʀᴠᴇʀ ʀᴜɴɴɪɴɢ ᴏɴ http://localhost:${PORT}
+╚──────────────────────╝
+`);
+});
+
+module.exports = app;
